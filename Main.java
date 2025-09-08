d64233c415b381009b51dd28af1c7773b96fc642
Andres Loreto Quiros <loretoa@javeriana.edu.co>
2025-09-07 21:12:03 -0500

    Enhance input handling in Main.java
    Refactor input handling to read from file or STDIN.

--- Main.java
+++ Main.java
@@ -1,11 +1,26 @@
+import java.nio.file.*;
+import java.io.*;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String input = "x = 5; imprimir(x);";
+        CharStream input;
 
-        MiLenguajeLexer lexer = new MiLenguajeLexer(CharStreams.fromString(input));
+        if (args.length > 0) {
+            // Leer desde archivo
+            input = CharStreams.fromPath(Path.of(args[0]));
+        } else {
+            // Leer desde STDIN
+            StringBuilder sb = new StringBuilder();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                String line;
+                while ((line = br.readLine()) != null) sb.append(line).append('\n');
+            }
+            input = CharStreams.fromString(sb.toString());
+        }
+
+        MiLenguajeLexer lexer = new MiLenguajeLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         MiLenguajeParser parser = new MiLenguajeParser(tokens);
 
